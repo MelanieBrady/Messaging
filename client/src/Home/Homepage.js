8198fcff725b45f643a6032f59987e0c156ef460
MelanieBrady <melaniebrady@knights.ucf.edu>
2020-11-28 11:22:27 -0500

    Revamping UI

--- client/src/Home/Homepage.js
+++ client/src/Home/Homepage.js
@@ -76,13 +76,13 @@ class HomePage extends React.Component {
             return (
                 <ul class="horizontal">
                     <Button style={{ float: 'left' }} variant="primary" size="sm"> My Profile </Button>
-                    <Form style={{ float: 'left' }} onSubmit={this.handleUsernameSearchSubmit}>
+                    <Form style={{ float: 'center' }} onSubmit={this.handleUsernameSearchSubmit}>
                         <Form.Control type="text" placeholder="Search for username..."
                             onChange={(e) => this.setState({ usernameSearch: e.target.value })} /> </Form>
-                    <Button style={{ float: 'left' }} variant="info" size="sm" type="submit">Search</Button>
+                    <Button style={{ float: 'center' }} variant="info" size="sm" type="submit">Search</Button>
 
-                    <Button style={{ float: 'left' }} variant="secondary" size="sm" onClick={this.handlePasswordReset}> Reset Password </Button>
-                    <Button style={{ float: 'left' }} variant="dark" size="sm" onClick={this.handleLogOutSubmit}> Log Out </Button>
+                    <Button style={{ float: 'right' }} variant="secondary" size="sm" onClick={this.handlePasswordReset} class="right"> Reset Password </Button>
+                    <Button style={{ float: 'right' }} variant="dark" size="sm" onClick={this.handleLogOutSubmit} class="right"> Log Out </Button>
                 </ul>
             );
 
