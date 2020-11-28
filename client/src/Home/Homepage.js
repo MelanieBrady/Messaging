5a35bb3c56cd339261a0e1b5e7eea14563d41469
MelanieBrady <melaniebrady@knights.ucf.edu>
2020-11-28 09:21:21 -0500

    Revamping UI

--- client/src/Home/Homepage.js
+++ client/src/Home/Homepage.js
@@ -68,42 +68,6 @@ class HomePage extends React.Component {
 
             return (
                 <div>
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Dropdown
-        </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" href="#">Disabled</a>
-                                </li>
-                            </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> </input>
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </nav>
-
                     <Form onSubmit={this.handleUsernameSearchSubmit}>
                         <Form.Group controlId="formUsernameSearch">
                             <Form.Control type="" placeholder="Search for Username"
@@ -125,15 +89,26 @@ class HomePage extends React.Component {
         } else {
             return (
                 <div>
-                    <p>this is the home page</p>
-                    <div className="menu">
-                        <ul>
-                            <li> <Link to="/login">this is a link to the login page</Link> </li>
-                            <li> <Link to="/register">this is a link to the register page</Link> </li>
-                            <li> <Link to="/profile/test">this is a link to the test page</Link> </li>
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <a class="navbar-brand" href="#"></a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                        </ul>
-                    </div>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active"> <Link to="/login">Login</Link> </li>
+                                <li class="nav-item"> <Link to="/register">Register</Link>
+                                </li>
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search for user" aria-label="Search"> </input>
+                                <Button variant="primary" block size="sm" type="submit" onClick={this.handleLogOutSubmit}>
+                                    Search
+                                </Button>
+                            </form>
+                        </div>
+                    </nav>
                 </div>
             );
         }
