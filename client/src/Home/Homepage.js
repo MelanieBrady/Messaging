cbaeda0c3c6146140cd600e16e4ad4792f51738e
MelanieBrady <melaniebrady@knights.ucf.edu>
2020-11-28 10:44:40 -0500

    Revamping UI

--- client/src/Home/Homepage.js
+++ client/src/Home/Homepage.js
@@ -70,10 +70,10 @@ class HomePage extends React.Component {
             return (
                 <div class="header">
                     <Button variant="primary" size="sm" type="submit"> My Profile </Button>
-                    <Form style={{ float: left }} onSubmit={this.handleUsernameSearchSubmit}>
+                    <Form style={{ float: 'left' }} onSubmit={this.handleUsernameSearchSubmit}>
                         <Form.Control type="text" placeholder="Search for username..."
                             onChange={(e) => this.setState({ usernameSearch: e.target.value })} /> </Form>
-                    <Button style={{ float: left }} variant="info" size="sm" type="submit">Search</Button>
+                    <Button style={{ float: 'left' }} variant="info" size="sm" type="submit">Search</Button>
 
                     <Button variant="secondary" size="sm" onClick={this.handlePasswordReset}> Reset Password </Button>
                     <Button variant="dark" size="sm" onClick={this.handleLogOutSubmit}> Log Out </Button>
