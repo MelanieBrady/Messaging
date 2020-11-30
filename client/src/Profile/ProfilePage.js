84e9aa938575180a3f14736c8c15c1a7c33365e2
MelanieBrady <melaniebrady@knights.ucf.edu>
2020-11-29 21:30:18 -0500

    Fixing up UI

--- client/src/Profile/ProfilePage.js
+++ client/src/Profile/ProfilePage.js
@@ -145,9 +145,9 @@ export default class ProfilePage extends React.Component {
                         <div className="smallLogo">
                             <img alt="logo" src="https://lh3.googleusercontent.com/hMjdvzKdf3jyhja4M7APZdeBv5PMXE9TgEY7cdGcmHvNj82x4594wKoTED-8fyXsH5oVnwTemRi1-2DBNA4WkxsSgbxEn8ctT7cXzcc67kKjdbscB4uPT9yG5MjOhrph_xHCGUoHh3bucMc_ueEaQMsavazPpg4x6b9ta7G8oevNcvoh4-6sMH7PjOtdN6G0Ed2UdP7gQOjs3FvG1CHLUlPJiZlno9Fpcqteg1Jfe_4YgxW5TSlgtYHEZrpV1j-JFFYg4-6H2_7UYXzzXKUD8sTvq8cbTygZKJYr_rTGrxKTDArldkjk-VrI0DVfZqKY7zJO4s7ZfNqeKZSQuEv29ih7s8HCHD_Q_od7DZheisutKlmIdrVQmlr2o51s6yIqmDwetMbcr1pkUTZwZU13CfrUVbGrEoPZmC4gY4W6M1cWl2LhGt_St9Hewkr860A_SJS7RTJp0P-p7YANHXDWy9StbQfLLHaqQFJgXzBNCJ7sw7vUsfGXFGoIfzvhO3cdaAb8pCrtUYvibgw9SVF5yR6B6GXUiBJ-BTrpN6CzLZrXr-MAxesPhXj5hUJ9B4pK0f2DDOc8ys_Ktx6dESVpSMQP0P6Wv0o0tQ2T_66YKRQxWpSoxoP7Dj7Bri5eZIBGsSBE3ajfXA0YkgiQpxQwa6mozZBquoxnUNXvdjDbH6NbJF2-QUknTz0fnYNLUw=w1015-h375-no?authuser=0" />
                         </div>
-                        <Button style={{ float: 'left' }} variant="primary" size="sm" onClick={this.handleMyProfile}> My Profile </Button>
-                        <Button style={{ float: 'right' }} variant="dark" size="sm" onClick={this.handleLogOutSubmit}> Log Out </Button>
-                        <Button style={{ float: 'right' }} variant="secondary" size="sm" onClick={this.handlePasswordReset}> Reset Password </Button>
+                        <Button style={{ float: 'left' }} type="button" variant="primary" size="sm" onClick={this.handleMyProfile}> My Profile </Button>
+                        <Button style={{ float: 'right' }} type="button" variant="dark" size="sm" onClick={this.handleLogOutSubmit}> Log Out </Button>
+                        <Button style={{ float: 'right' }} type="button" variant="secondary" size="sm" onClick={this.handlePasswordReset}> Reset Password </Button>
                     </ul>
                     <ul className="horizontal_SecondRow">
                         <Form style={{ float: 'left' }} size="sm" onSubmit={this.handleUsernameSearchSubmit}>
@@ -165,8 +165,8 @@ export default class ProfilePage extends React.Component {
                                         <div class="mt-3">
                                             <h4> {name} </h4>
                                             <h6> {this.state.username} </h6>
-                                            <Button block size="sm" style={{ display: 'inlineBlock' }} onClick={this.handleFavoriteButtonClick}> Favorite </Button>
-                                            <Button block size="sm" variant="outlinePrimary" style={{ display: 'inlineBlock' }} onClick={this.handleMessageButtonClick}> Message </Button>
+                                            <Button block size="sm" type="button" style={{ display: 'inlineBlock' }} onClick={this.handleFavoriteButtonClick}> Favorite </Button>
+                                            <Button block size="sm" type="button" variant="outlinePrimary" style={{ display: 'inlineBlock' }} onClick={this.handleMessageButtonClick}> Message </Button>
                                         </div>
                                     </div>
                                 </div>
